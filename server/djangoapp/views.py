ad741b74f76c5f339496a3b80ace09dca05023ac
Do Phuong Anh <phuonganhnniie@Dos-MacBook-Air.netis.cc>
2024-01-30 23:00:51 +0700

    feat: add about & contact templates

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -13,18 +13,18 @@ import json
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
-
-# Create your views here.
-def my_static_view(request):
-    return render(request, template_name='static_template.html')
-
 # Create an `about` view to render a static about page
-# def about(request):
-# ...
+def about(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/about.html', context)
 
 
 # Create a `contact` view to return a static contact page
-#def contact(request):
+def contact(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/contact.html', context)
 
 # Create a `login_request` view to handle sign in request
 # def login_request(request):
